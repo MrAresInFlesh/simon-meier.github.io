8ce631e115f2bd843cd66646607d3221a1fd43c2
Simon Meier <simon.meier@he-arc.ch>
2022-07-05 13:02:51 +0200

    Add components project.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,10 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Simon Meier',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Simon Meier is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    siteUrl: 'https://simonmeier.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'SimonMeier',
+        short_name: 'SimonMeier',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
