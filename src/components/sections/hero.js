21187913ca35229c5bc7eeaf89024476e354b278
Simon Meier <simon.meier333@gmail.com>
2023-09-11 20:40:06 +0200

    Changing min height.

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -10,7 +10,7 @@ const StyledHeroSection = styled.section`
   ${({ theme }) => theme.mixins.flexCenter};
   flex-direction: column;
   align-items: flex-start;
-  min-height: 140vh;
+  min-height: 110vh;
   padding: 0;
 
   @media (max-width: 480px) and (min-height: 700px) {
